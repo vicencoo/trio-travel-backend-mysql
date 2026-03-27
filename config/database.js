227ac0c0e6747e4becf6dbee33a616805f821b9b
“vicencoo” <vicencollanaj2004@gmail.com>
2026-03-27 12:28:49 +0100

    fixed supabase

--- config/database.js
+++ config/database.js
@@ -16,10 +16,12 @@
 require('dotenv').config({ quiet: true });
 
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl: {
