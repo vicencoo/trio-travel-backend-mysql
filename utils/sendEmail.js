1a13201ea96151b17ba7603de1b6fa946feb62ed
vicencoo <vicencollanaj2004@gmail.com>
2026-09-16 11:29:41 +0200

    fixed insurance reminder

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,14 +2,10 @@ const nodemailer = require("nodemailer");
 
 async function sendEmail(to, subject, html, from = process.env.EMAIL_USER) {
   try {
-    console.log("Sending Insurance reminder started");
-
     if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
       console.error("Email credentials missing");
       return;
     }
-    console.log("Started authentication...");
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -18,16 +14,12 @@ async function sendEmail(to, subject, html, from = process.env.EMAIL_USER) {
       },
     });
 
-    console.log("Passed authentication and start sendin email");
-
     await transporter.sendMail({
       from,
       to,
       subject,
       html,
     });
-
-    console.log("Sendin email finished!!!");
   } catch (err) {
     console.error("Email send failed:", err);
   }
