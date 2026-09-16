1da5ca5df9df07ae6b53a12a3ffe383db9214c66
vicencoo <vicencollanaj2004@gmail.com>
2026-09-16 11:49:07 +0200

    updated sendEmail function

--- api/cron/insurance-reminder.js
+++ api/cron/insurance-reminder.js
@@ -1,11 +1,13 @@
-const runInsuranceReminder = require('../../utils/insuranceReminder');
+const runInsuranceReminder = require("../../utils/insuranceReminder");
 
 module.exports = async (req, res) => {
   try {
+    console.log("Insurance reminder started executing");
+
     await runInsuranceReminder();
-    res.status(200).json({ message: 'Reminder executed successfully' });
+    res.status(200).json({ message: "Reminder executed successfully" });
   } catch (err) {
-    console.error('CRON ERROR:', err.message);
+    console.error("CRON ERROR:", err.message);
     console.error(err.stack);
     res.status(500).json({ message: err.message });
   }
